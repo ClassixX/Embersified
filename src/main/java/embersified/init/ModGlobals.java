43ddf9267c068d69e3eee676d3a09679817ce047
ClassixX <30446897+ClassixX@users.noreply.github.com>
2024-03-10 09:19:23 +0100

    Added Funnel and Ejector recipes
    Signed-off-by: ClassixX <30446897+ClassixX@users.noreply.github.com>

--- src/main/java/embersified/init/ModGlobals.java
+++ src/main/java/embersified/init/ModGlobals.java
@@ -8,7 +8,7 @@ public class ModGlobals {
 
 	public static final String MODID = "embersifiedextended";
 	public static final String NAME = "EmbersifiedExtended";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String DEPENDENCIES = "required-after:embers@[1.8,);";
 	public static final String CONFIG_FILE = NAME + ".cfg";
 
